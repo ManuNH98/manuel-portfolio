7f76e75af07acb7f75b7e6874b4560656c2de678
ManuNH98 <manuelnhc@gmail.com>
2024-06-03 19:25:28 +0100

    FINISH Footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    screens:{
+      'microsm': '321px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '4xl': '2559px',
+    },
     colors:{
       transparent: 'transparent',
       current: 'currentColor',
